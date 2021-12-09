0fc81e1d8521777f8de63c4e348e5859cb2e9692
tangzc <imtzc@qq.com>
2021-12-09 10:55:58 +0800

    fix：修复@TablePrimary注解不能用在类上的bug

--- mybatis-plus-ext-actable/mybatis-plus-ext-actable-annotation/src/main/java/com/tangzc/mpe/actable/annotation/TablePrimary.java
+++ mybatis-plus-ext-actable/mybatis-plus-ext-actable-annotation/src/main/java/com/tangzc/mpe/actable/annotation/TablePrimary.java
@@ -15,7 +15,7 @@ import java.lang.annotation.Target;
  * @version 2020年11月11日 下午6:13:37
  */
 //表示注解加在接口、类、枚举等
-@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 //VM将在运行期也保留注释，因此可以通过反射机制读取注解的信息
 @Retention(RetentionPolicy.RUNTIME)
 //将此注解包含在javadoc中
