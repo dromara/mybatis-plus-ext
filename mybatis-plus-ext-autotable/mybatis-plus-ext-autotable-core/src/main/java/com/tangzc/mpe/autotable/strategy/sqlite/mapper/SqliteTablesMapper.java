dec05089b74da1f34486f23a4a086ec1a5e609c5
唐振超 <imtzc@qq.com>
2023-05-31 11:14:47 +0800

    修复bug：#I79ID8

--- mybatis-plus-ext-autotable/mybatis-plus-ext-autotable-core/src/main/java/com/tangzc/mpe/autotable/strategy/sqlite/mapper/SqliteTablesMapper.java
+++ mybatis-plus-ext-autotable/mybatis-plus-ext-autotable-core/src/main/java/com/tangzc/mpe/autotable/strategy/sqlite/mapper/SqliteTablesMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import com.tangzc.mpe.autotable.strategy.sqlite.data.dbdata.SqliteMaster;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -55,6 +57,13 @@ public interface SqliteTablesMapper {
      * @param tableName 表名
      * @return 建表语句
      */
+    @Results({
+            @Result(column = "type", property = "type"),
+            @Result(column = "name", property = "name"),
+            @Result(column = "tbl_name", property = "tblName"),
+            @Result(column = "rootpage", property = "rootpage"),
+            @Result(column = "sql", property = "sql"),
+    })
     @Select("select * from sqlite_master where type='index' and tbl_name=#{tableName};")
     List<SqliteMaster> queryBuildIndexSql(String tableName);
 
