fad4ee4627d99cf6849bd4535b4680dd4dc60f94
唐振超 <imtzc@qq.com>
2023-12-03 17:42:51 +0800

    添加注释

--- mybatis-plus-ext-autotable/mybatis-plus-ext-autotable-core/src/main/java/com/tangzc/mpe/autotable/strategy/mysql/converter/JavaToMysqlConverter.java
+++ mybatis-plus-ext-autotable/mybatis-plus-ext-autotable-core/src/main/java/com/tangzc/mpe/autotable/strategy/mysql/converter/JavaToMysqlConverter.java
@@ -3,6 +3,7 @@ package com.tangzc.mpe.autotable.strategy.mysql.converter;
 import com.tangzc.mpe.autotable.strategy.mysql.data.MysqlTypeAndLength;
 
 /**
+ * 自定义java转MySQL的类型转换器
  * @author don
  */
 @FunctionalInterface
