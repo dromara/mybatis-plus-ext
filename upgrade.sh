392d15e551a58605a99e053975663b4e5d6ffac1
tangzc <imtzc@qq.com>
2024-03-05 14:15:00 +0800

    优化升级文件

--- upgrade.sh
+++ upgrade.sh
@@ -5,7 +5,7 @@ echo "开始替换pom.xml的版本号：${version}"
 mvn versions:set -DnewVersion=${version}
 
 echo "开始commit到本地仓库：${version}"
-git commit -am "版本升级：${version}"
+git commit -m "版本升级：${version}"
 
 echo "开始打tag：v${version}"
 git tag -a v${version} -m "版本号：${version}"
@@ -14,4 +14,4 @@ echo "开始提交到远程git仓库：${version}"
 git push origin main --tags
 
 echo "开始发布新的版本到maven仓库：${version}"
-mvn clean deploy -Dmaven.test.skip=true -pl mybatis-plus-ext-automapper,mybatis-plus-ext-autotable-core,mybatis-plus-ext-bind-core,mybatis-plus-ext-core-base,mybatis-plus-ext-condition-core,mybatis-plus-ext-datasource-core,mybatis-plus-ext-spring-boot-starter,mybatis-plus-ext-spring-boot3-starter -am
+mvn -X clean deploy -Dmaven.test.skip=true -pl mybatis-plus-ext-automapper,mybatis-plus-ext-autotable,mybatis-plus-ext-bind,mybatis-plus-ext-core,mybatis-plus-ext-condition,mybatis-plus-ext-datasource,mybatis-plus-ext-spring-boot-starter,mybatis-plus-ext-spring-boot3-starter -am
