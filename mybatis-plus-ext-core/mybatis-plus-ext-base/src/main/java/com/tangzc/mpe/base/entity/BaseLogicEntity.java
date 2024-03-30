25b2c06657e548c167e6790197e6c1f5c7eaccf6
tangzc <imtzc@qq.com>
2024-03-30 11:31:28 +0800

    :sparkles: 修改BaseLogicEntity中deleted默认值的标注方式，改为数据库层面的默认值

--- mybatis-plus-ext-core/mybatis-plus-ext-base/src/main/java/com/tangzc/mpe/base/entity/BaseLogicEntity.java
+++ mybatis-plus-ext-core/mybatis-plus-ext-base/src/main/java/com/tangzc/mpe/base/entity/BaseLogicEntity.java
@@ -2,7 +2,7 @@ package com.tangzc.mpe.base.entity;
 
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.tangzc.autotable.annotation.ColumnComment;
-import com.tangzc.mpe.annotation.DefaultValue;
+import com.tangzc.autotable.annotation.ColumnDefault;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,7 +16,7 @@ import java.io.Serializable;
 public class BaseLogicEntity<ID_TYPE extends Serializable, TIME_TYPE> extends BaseEntity<ID_TYPE, TIME_TYPE> {
 
     @TableLogic
-    @DefaultValue("0")
+    @ColumnDefault("0")
     @ColumnComment("逻辑删除标志")
     protected Integer deleted;
 }
